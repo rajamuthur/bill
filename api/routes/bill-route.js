b6cb329a0ae31d2cc69e4f8ccf05077454b6e2c4
Rajamuthu Ramasamy <rajamuthu.ramasamy@impigertech.com>
2019-07-09 21:25:25 +0530

    Check token on bill related api

--- api/routes/bill-route.js
+++ api/routes/bill-route.js
@@ -3,13 +3,13 @@ var bills = require('../controllers/bill-controller');
 var utilities = require('../../common/utilities');
 
 module.exports = function (router) {
-    router.get('/bills/:id', bills.getBill);
-    router.get('/bills/',  bills.getAllBills);
-    router.post('/bills/add/', bills.addBill);
+    router.get('/bills/:id', utilities.verifyToken, bills.getBill);
+    router.get('/bills/', utilities.verifyToken, bills.getAllBills);
+    router.post('/bills/add/', utilities.verifyToken, bills.addBill);
     router.post('/bills/searchwithlimit/', utilities.verifyToken, bills.getBillBySearchWithLimit);
-    router.post('/bills/search/', bills.getBillsBySearch);
-    router.post('/bills/:id',bills.updateBill);
-    router.delete('/bills/delete/:id', bills.deleteBill);
+    router.post('/bills/search/', utilities.verifyToken, bills.getBillsBySearch);
+    router.post('/bills/:id', utilities.verifyToken, bills.updateBill);
+    router.delete('/bills/delete/:id', utilities.verifyToken, bills.deleteBill);
     // router.get('/bills/:id', utilities.verifyToken, bills.getBill);
     // router.get('/bills/', utilities.verifyToken, bills.getAllBills);
     // router.post('/bills/add/', utilities.verifyToken, bills.addBill);
